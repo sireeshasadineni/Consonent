adcf28f0ea28ce28ff7f21aaed2cbfc48e3272aa
sireeshasadineni <41733085+sireeshasadineni@users.noreply.github.com>
2018-07-26 17:14:39 -1200

    Create consonent.java

---
+++ consonent.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='z';
+		{
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch =='u' )
+			System.out.println("Vowel");
+			else if(ch>='a'&&ch<='z')
+			System.out.println("Consonant");
+			else
+			System.out.println("invalid");
+		
+		}
+	}
+ }
